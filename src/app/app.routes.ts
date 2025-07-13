b7776f2133ce271373494616d8c63494baaa0d11
Ashley L. <laboryashley@gmail.com>
2025-07-14 01:08:36 +0200

    routing added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import {LandingPage} from './shared/components/landing-page/landing-page';
+import {ProductDetails} from './shared/components/product-details/product-details';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: LandingPage},
+  { path: 'product/:id', component: ProductDetails }
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,19 +1,13 @@
 import { Component } from '@angular/core';
 import { Header } from './layout/header/header';
-import { HeroCarousel } from './shared/components/hero-carousel/hero-carousel';
-import { ProductCardList } from './shared/components/product-card-list/product-card-list';
+import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, HeroCarousel, ProductCardList],
+  imports: [Header, RouterOutlet],
   templateUrl: './app.html',
   styleUrls: ['./app.scss'],
 })
 export class App {
-  images: string[] = [
-    'https://plus.unsplash.com/premium_photo-1668416114981-1d6cd2acbc7d?...',
-    'https://plus.unsplash.com/premium_photo-1661598195527-7eb77099ce35?...',
-    'https://plus.unsplash.com/premium_photo-1665408511441-cf99b0f6f954?...'
-  ];
 }
